91aa2936fbc63b228353061a06aa075e05d3bb12
mariaguzman1120 <mariaguzman1120@gmail.com>
2023-09-06 22:06:41 -0500

    feat: archivos de configuracion

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='unsupervised',
+    version='0.1.0',
+    description='Functions for applying dimensionality reduction',
+    author='Maria del mar',
+    author_email='example@email.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=[
+        'unsupervised',
+        'unsupervised.dimensionality_reduction'],
+    install_requires=['numpy'],
+)
---
+++ unsupervised/dimensionality_reduction/__init__.py
@@ -0,0 +1,2 @@
+from unsupervised.dimensionality_reduction.pca import *
+from unsupervised.dimensionality_reduction.svd import *
